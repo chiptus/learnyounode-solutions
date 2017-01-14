3fa25a98e3632c671fdfdab1b35b0ace0abd38f6
Chaim Lando <chiptus@gmail.com>
2017-01-14 14:11:09 +0000

    finishes http-uppercaser

---
+++ http-uppercaser.js
@@ -0,0 +1,13 @@
+//imports 
+var map = require("through2-map");
+
+const port = require("./modules/getport")();
+const {createServerAndListen} = require("./modules/httpClient");
+
+createServerAndListen(port, (req, res) => {
+   if (req.method !== "POST") {
+       return;
+   } 
+   req.pipe(map({wantStrings: true},chunk => chunk.toUpperCase())).pipe(res);
+});
+
